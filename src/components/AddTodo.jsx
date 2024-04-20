c5a664369f72e97f4a48246bef795db524138f16
divya562 <divyabagul2502@gmail.com>
2024-04-20 14:02:35 +0530

    using hook useRef

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,33 +1,30 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 const Addtodo = ({addNewItem}) =>{
-  const[todoName, setTodoName]=useState("");
-  const[todoDate, setTodoDate]=useState("");
+  
 
-  const handleName=(event)=>{
-    setTodoName(event.target.value)
+  const todoNameElement=useRef();
+  const dueDateElement=useRef();
 
-  }
-  const handleDate=(event)=>{
-    setTodoDate(event.target.value);
-
-  }
 
   const handleAddbtnClick=(event)=>{
     event.preventDefault();
+    const todoName=todoNameElement.current.value;
+    const todoDate = dueDateElement.current.value;
+    todoNameElement.current.value=""
+    dueDateElement.current.value=""
     addNewItem(todoName,todoDate);
-    setTodoName("");
-    setTodoDate("");
+
   }
 
     return(
    <div className="container text-center" style={{width:"700px"}}>
         <form className="row gy-2 m-4" onSubmit={handleAddbtnClick}>
         <div className="col-6">
-          <input type="text" className="form-control" id="autoSizingInput" value={todoName} placeholder="Enter Todo Here" onChange={handleName}/>
+          <input type="text" ref={todoNameElement} className="form-control" id="autoSizingInput"  placeholder="Enter Todo Here" />
         </div>
         <div className="col-4" >
-          <input type="date"className="form-control" value={todoDate}  onChange={handleDate}/>
+          <input type="date" ref={dueDateElement} className="form-control"   />
         </div>
         <div className="col-2">
           <button type="submit" className="btn btn-success" style={{width:"100px"}} onClick={handleAddbtnClick}>Add</button>
